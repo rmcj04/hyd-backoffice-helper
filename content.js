f1e73146c0246ed2d17d8efc65dd9540a9edb544
rmcj <evgeniyrmcj@gmail.com>
2025-10-31 14:34:45 +0300

    fix due frontend updated

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ function waitForElement(selector, callback) {
 }
 
 function checkIsPaymentsPage() {
-    let targetSelector = "#R7302135388147866 > div.t-BreadcrumbRegion-top > div.t-BreadcrumbRegion-body > div.t-BreadcrumbRegion-breadcrumb > ul > li > h1";
+    let targetSelector = "#R124883111009762779 > div.t-BreadcrumbRegion-top > div.t-BreadcrumbRegion-body > div.t-BreadcrumbRegion-breadcrumb > ul > li > h1";
     return !!document.querySelector(targetSelector);
 }
 
